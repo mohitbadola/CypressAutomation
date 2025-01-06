83a499d2b48efdaf6d8aadb1d6e80c341fc43672
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-06 20:35:24 +0530

    Add test for product search

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -4,10 +4,30 @@
 /// <reference types="cypress" />
 
 describe('My First Test Suite', () => {
-    it('My First Test case', () => {
+    it('Test case for Product Search', () => {
         //test step
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
         cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        //selenium get hit url in browser, cypress get acts like findElement of selenium
+        cy.get('.product').should('have.length', 5)
+        cy.get('.product:visible').should('have.length', 4)
+
+        cy.get('.products .product').should('have.length', 4)
+
+        // Parent child chaining
+        cy.get('.products').find('.product').should('have.length', 4)
+
+        cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click()
+
+        cy.get('.products .product').eq(2).contains('ADD TO CART').click()
+
+        cy.wait(2000)
+
+        cy.get('.products .product').eq(2).should('contain.text', 'ADDED');
+
+
+        cy.get('.cart-icon > img').click()
 
     })
   })
