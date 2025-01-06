424c369f65a5249e6d9e730f873355056983ca5c
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-07 01:26:45 +0530

    Handling Async promises and create alias

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -15,11 +15,17 @@ describe('My First Test Suite', () => {
 
         cy.get('.products .product').should('have.length', 4)
 
+
         // Parent child chaining
-        cy.get('.products').find('.product').should('have.length', 4)
+        cy.get('.products').as('productLocator')
+
+        cy.get('@productLocator').find('.product').should('have.length', 4)
+        // cy.get('.products').find('.product').should('have.length', 4)
+
+        cy.get('@productLocator').find('.product').eq(1).contains('ADD TO CART').click()
 
         // cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click()
-        // cy.get('.products .product').eq(2).contains('ADD TO CART').click()
+        cy.get('.products .product').eq(1).contains('ADD TO CART').click()
         // cy.get('.products .product').eq(2).should('contain.text', 'ADDED')
 
         cy.get('.products .product').each(($el, $list) => {
@@ -30,6 +36,13 @@ describe('My First Test Suite', () => {
                 cy.wrap($el).find('button').click();
             }
         })
-        // cy.get('.products .product').eq(2).should('contain.text', 'ADDED')
+      
+        //assert if logo text is correctly displayed
+        cy.get('.brand').should('have.text','GREENKART')
+
+        //this is to print in logs
+        cy.get('.brand').then(function(logoelement){
+            cy.log(logoelement.text())
+        })
     })
   })
