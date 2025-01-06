950332a4c13605fa109c4bea9e526c73ce48c698
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-07 02:06:03 +0530

    Create placing order test

---
+++ cypress/integration/examples/Test2.js
@@ -0,0 +1,30 @@
+//cypress - Spec
+
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+describe('Test Suite', () => {
+    it('Test placing order', () => {
+        //test step
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        // Parent child chaining
+        cy.get('.products').as('productLocator')
+      
+        cy.get('@productLocator').find('.product').each(($el, $list)=>{
+            const textVeg = $el.find('h4.product-name').text()
+            if(textVeg.includes('Capsicum')){
+                cy.wrap($el).find('button').click()
+            }
+        })
+
+        cy.get('.cart-icon > img').click()
+        cy.contains('PROCEED TO CHECKOUT').click()
+        cy.contains('Place Order').click()
+
+        cy.get('select').select('India')
+        cy.get('.chkAgree').click()
+        cy.get('button').click()
+    })
+  })
