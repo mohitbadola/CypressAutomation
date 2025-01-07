2d0f9eab61b7e52d42297679b37ac675aac03f51
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-07 16:42:17 +0530

    test static dropdown

--- cypress/integration/examples/test3.js
+++ cypress/integration/examples/test3.js
@@ -1,12 +1,17 @@
 /// <reference types="cypress" />
 
 describe('test suite', ()=>{
-    it('test checkboxes', ()=>{
+    it('test checkboxes and dropdown', ()=>{
 
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+
+        //check boxes
         cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked').and('have.value', 'option1')
 
         cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+
+        //Static dropdown
+        cy.get('select').select('option2').should('have.value', 'option2')
     })
 })
