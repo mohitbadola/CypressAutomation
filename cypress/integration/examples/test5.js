f528cb7fb4f9348e8ff81d5e8c8fea1ba66eac7e
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-07 22:09:34 +0530

    handle child window test

---
+++ cypress/integration/examples/test5.js
@@ -0,0 +1,13 @@
+describe('Handling Child Windows', () => {
+  it('Should handle Child Window', ()=>{
+
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+    cy.get('#opentab').invoke('removeAttr', 'target').click();
+
+    cy.origin("https://www.qaclickacademy.com/", ()=>{
+        cy.get("#navbarSupportedContent  a[href*='about']").click();
+        cy.get('.mt-50 h2').should('contain', 'Welcome to QAClick Academy')
+    })
+  
+  })
+})
