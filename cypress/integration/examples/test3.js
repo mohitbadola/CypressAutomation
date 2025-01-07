9a28c14f1a37dcb58eb410cfff333388f2cd80c9
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-07 17:35:13 +0530

    Handle dynamic dropdown

--- cypress/integration/examples/test3.js
+++ cypress/integration/examples/test3.js
@@ -5,13 +5,23 @@ describe('test suite', ()=>{
 
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
 
-        //check boxes
-        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
-        cy.get('#checkBoxOption1').uncheck().should('not.be.checked').and('have.value', 'option1')
+        // //check boxes
+        // cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
+        // cy.get('#checkBoxOption1').uncheck().should('not.be.checked').and('have.value', 'option1')
 
-        cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+        // cy.get('input[type="checkbox"]').check(['option2', 'option3'])
 
-        //Static dropdown
-        cy.get('select').select('option2').should('have.value', 'option2')
+        // //Static dropdown
+        // cy.get('select').select('option2').should('have.value', 'option2')
+
+        //Dynamic dropdown
+        cy.get('#autocomplete').type('Ind')
+
+        cy.get('.ui-menu-item div').each(($el, $list)=>{
+            if($el.text() === 'India'){
+                cy.wrap($el).click()
+            }
+        })
+        cy.get('#autocomplete').should('have.value', 'India')
     })
 })
