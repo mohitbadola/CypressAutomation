5c471bc8be8b793ec735e2ede9f51d72a7c245d2
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-08 21:05:59 +0530

    Update Submit order test with filter

--- cypress/integration/examples/Test12Framework.js
+++ cypress/integration/examples/Test12Framework.js
@@ -1,10 +1,17 @@
 describe('End to Eed ecommerce test', ()=>{
     it('Submit Order', ()=>{
+
+        const productName = 'Nokia Edge'
         cy.visit('https://rahulshettyacademy.com/loginpagePractise/#')
         cy.get('#username').type('rahulshettyacademy')
         cy.get('#password').type('learning')
         cy.contains('Sign In').click()
         cy.contains('Shop Name').should('be.visible')
         cy.get('app-card').should('have.length', 4)
+
+        cy.get('app-card').filter(`:contains("${productName}")`).then($element=>{
+            cy.wrap($element).should('have.length', 1)
+            cy.wrap($element).contains('button', 'Add').click()
+        })
     })
 })
