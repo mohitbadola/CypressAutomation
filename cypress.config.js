a9cfbc641713767f8d99d4acf9a456018e41304c
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-10 02:47:01 +0530

    add retries

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,10 @@ module.exports = defineConfig({
   env:{
     url: "https://rahulshettyacademy.com"
   },
+  // retries: {
+  //   runMode: 1
+  // },
+  // projectId: "nodpcq",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
