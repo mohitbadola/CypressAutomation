da56839fd1cef24665871aa444abf203f6411370
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-12 01:22:19 +0530

    Call cypress task into test with multiple parameters

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 const excelToJson = require('convert-excel-to-json');
 const fs = require('fs');
+const ExcelJs = require("exceljs");
 
 const browserify = require("@cypress/browserify-preprocessor");
 const {
@@ -24,6 +25,20 @@ async function setupNodeEvents(on, config) {
   );
 
   on('task', {
+   async writeExcelTest({searchText, replaceText, change, filePath}) {
+      const workbook = new ExcelJs.Workbook();
+      await workbook.xlsx.readFile(filePath);
+      const worksheet = workbook.getWorksheet("Sheet1");
+      const output = await readExcel(worksheet, searchText);
+    
+      const cell = worksheet.getCell(output.row, output.column+change.colChange);
+      cell.value = replaceText;
+      await workbook.xlsx.writeFile(filePath);
+    }
+  })
+
+
+  on('task', {
     excelToJsonConverter(filePath){
       const result = excelToJson({
         source: fs.readFileSync(filePath) // fs.readFileSync return a Buffer
@@ -33,6 +48,19 @@ async function setupNodeEvents(on, config) {
   })
 
   return config;
+}
+
+async function readExcel(worksheet, searchText) {
+  let output = { row: -1, column: -1 };
+  worksheet.eachRow((row, rowNumber) => {
+    row.eachCell((cell, colNumber) => {
+      if (cell.value === searchText) {
+        output.row = rowNumber;
+        output.column = colNumber;
+      }
+    });
+  });
+  return output;
 }
 
 module.exports = defineConfig({
---
+++ cypress/integration/examples/ExcelTest.js
@@ -0,0 +1,28 @@
+const ExcelJs = require("exceljs");
+
+async function readExcel(worksheet, searchText) {
+    let output = { row: -1, column: -1 };
+    worksheet.eachRow((row, rowNumber) => {
+      row.eachCell((cell, colNumber) => {
+        if (cell.value === searchText) {
+          output.row = rowNumber;
+          output.column = colNumber;
+        }
+      });
+    });
+    return output;
+  }
+
+async function writeExcelTest(searchText, replaceText, change, filePath) {
+  const workbook = new ExcelJs.Workbook();
+  await workbook.xlsx.readFile(filePath);
+  const worksheet = workbook.getWorksheet("Sheet1");
+  const output = await readExcel(worksheet, searchText);
+
+  const cell = worksheet.getCell(output.row, output.column+change.colChange);
+  cell.value = replaceText;
+  await workbook.xlsx.writeFile(filePath);
+}
+
+// update Mango price to 350
+writeExcelTest("Mango", 350, {rowChange:0, colChange:2}, "downloads/exceldownloadTest.xlsx");
